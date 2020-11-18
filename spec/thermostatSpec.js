1c4ed9cdf5b69fb3b089567567dc8560acda99d2
Ben Jones <benwjones76@outlook.com>
2020-11-18 11:54:37 +0000

    added min temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,9 +7,30 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
+
   // Thermostat starts at 20 degrees
   it('starts the thermostat at 20 degrees', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  // You can increase the temperature with an up function
+  it('increases the temperature by 1', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  // You can decrease the temperature with a down function
+  it('decreases the temperature by 1', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  // The minimum temperature is 10 degrees
+  it('stops the temperature going below 10', function() {
+    for (var i = 0; i < 11; i++) {
+    thermostat.down();
+  }
+  expect(thermostat.getCurrentTemperature()).toEqual(10);
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,8 +3,21 @@
 class Thermostat {
   constructor() {
     this.temperature = 20;
+    this._MIN_TEMP = 10;
   };
+
   getCurrentTemperature() {
     return this.temperature;
   };
+
+  up() {
+    this.temperature++;
+  };
+
+  down() {
+    if (this.temperature > this._MIN_TEMP) {
+      this.temperature--;
+    };
+  }
+
 };
